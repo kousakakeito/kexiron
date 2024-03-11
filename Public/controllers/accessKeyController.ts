04192e092e376de903c609c2a4e01cbf63a90459
kousakakeito <139373779+kousakakeito@users.noreply.github.com>
2024-03-11 23:48:42 +0900

    アクセスキー仕様変更

--- Public/controllers/accessKeyController.ts
+++ Public/controllers/accessKeyController.ts
@@ -4,7 +4,8 @@ const router: Router = Router();
 router.post('/verify-key', (req, res) => {
     const { key } = req.body;
     const currentDate = new Date();
-    const formattedDate = `${currentDate.getFullYear()}${(currentDate.getMonth() + 1).toString().padStart(2, "0")}${currentDate.getDate().toString().padStart(2, "0")}${currentDate.getHours().toString().padStart(2, "0")}${currentDate.getMinutes().toString().padStart(2, "0")}`;
+    currentDate.setHours(currentDate.getHours() + 9);
+    const formattedDate = `${currentDate.getFullYear()}${(currentDate.getMonth() + 1).toString().padStart(2, '0')}${currentDate.getDate().toString().padStart(2, '0')}${currentDate.getHours().toString().padStart(2, '0')}${currentDate.getMinutes().toString().padStart(2, '0')}`;
     
     console.log(key)
     console.log(formattedDate)
