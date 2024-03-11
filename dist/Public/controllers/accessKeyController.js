b3218281f9d5bb90aa403ab27df4237e5e350666
kousakakeito <139373779+kousakakeito@users.noreply.github.com>
2024-03-11 23:52:22 +0900

    アクセスキー仕様変更

--- dist/Public/controllers/accessKeyController.js
+++ dist/Public/controllers/accessKeyController.js
@@ -5,7 +5,8 @@ const router = (0, express_1.Router)();
 router.post('/verify-key', (req, res) => {
     const { key } = req.body;
     const currentDate = new Date();
-    const formattedDate = `${currentDate.getFullYear()}${(currentDate.getMonth() + 1).toString().padStart(2, "0")}${currentDate.getDate().toString().padStart(2, "0")}${currentDate.getHours().toString().padStart(2, "0")}${currentDate.getMinutes().toString().padStart(2, "0")}`;
+    currentDate.setHours(currentDate.getHours() + 9);
+    const formattedDate = `${currentDate.getFullYear()}${(currentDate.getMonth() + 1).toString().padStart(2, '0')}${currentDate.getDate().toString().padStart(2, '0')}${currentDate.getHours().toString().padStart(2, '0')}${currentDate.getMinutes().toString().padStart(2, '0')}`;
     console.log(key);
     console.log(formattedDate);
     if (key === formattedDate) {
